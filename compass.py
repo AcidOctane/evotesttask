7fd0e125283fc4b859f6aa405963b61511224acd
ArtemSydorenko <s1dneyoctane@gmail.com>
2022-01-20 18:31:49 +0200

    Add comments

--- compass.py
+++ compass.py
@@ -1,13 +1,15 @@
 def direction(facing, turn):
-    DIRECTIONS = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
     try:
+        #calculate number of 45 degrees turns
         turns = turn // 45
- 
-        start = DIRECTIONS.index(facing)
-        end = (start + turns) % len(DIRECTIONS) 
-        return DIRECTIONS[end]
+        #find corresponding index of the direction 
+        start = directions.index(facing)
+        #calculate the resulting direction
+        end = (start + turns) % len(directions) 
+        return directions[end]
     except TypeError:
-        print("Turn angle must be integer value between -1080 nad 1080")
-        print("Poosible starting directions are: N, NE, E, SE, S, SW, W, NW")
+        print("Turn angle must be integer value between -1080 and 1080")
     except ValueError:
         print(f"ValueError: {facing} is not in list")
+        print("Poosible starting directions are: N, NE, E, SE, S, SW, W, NW")
